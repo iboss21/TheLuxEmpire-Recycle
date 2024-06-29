72096570af2e03794f4e79ec14185036e28660cc
Master Mind <48193931+iboss21@users.noreply.github.com>
2024-06-28 23:11:10 -0600

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,30 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'TheLuxEmpire Recycling System'
+version '1.0.0'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    '@qb-core/shared/locale.lua',
+    'locales/en.lua',
+    'config.lua'
+}
+
+client_scripts {
+    'client/main.lua',
+    'client/menus.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/main.lua'
+}
+
+lua54 'yes'
+
+dependencies {
+    'qb-core',
+    'qb-menu',
+    'ox_lib'
+}
